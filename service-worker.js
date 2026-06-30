4809b753c8ed55c38542bca38048d9179e79ef8f
Philipbo <philipbo@gmail.com>
2026-06-30 13:46:13 +0400

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,4 +0,0 @@
-const CACHE='atlas-pwa-v3';
-const ASSETS=['./','./index.html','./style.css','./app.js','./manifest.json','./data/diagrams.json','./icons/icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
